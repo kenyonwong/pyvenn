d383897985ded6416c75675a4efaa00c12132c9a
kenyonwong <kenyonispig@yahoo.com.hk>
2018-10-04 14:13:17 +0800

    Add a fill called list

--- venn.py
+++ venn.py
@@ -110,7 +110,7 @@ def get_labels(data, fill=["number"]):
     labels = []
     if "logic" in fill:
         for k in set_collections:
-            labels[k] = k + ": "
+            labels[k] = []
     if "number" in fill:
         for k in set_collections:
             labels[k] += str(len(set_collections[k]))
